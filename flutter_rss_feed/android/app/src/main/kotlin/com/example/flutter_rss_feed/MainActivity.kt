c76991314b94c7dd077ff2a0445c49f3a17ce5be
antonwentzel <anton@idigisolweb.com>
2023-08-25 09:26:58 +0200

    rss upload

---
+++ flutter_rss_feed/android/app/src/main/kotlin/com/example/flutter_rss_feed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_rss_feed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_rss_feed/lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:xml2json/xml2json.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+ 
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ final Xml2Json xml2json = Xml2Json(); 
+ List topStories =[];
+
+Future getArticles()async{
+
+  final url = Uri.parse( 'https://www.rugbypass.com/feeds/rss/');
+
+  final response = await http.get(url);
+
+  xml2json.parse(response.body);
+
+  var jsondata = await xml2json.toGData();
+  var data = json.decode(jsondata);
+
+  topStories = data['rss']['channel']['item'];
+
+  print(topStories);
+
+
+}
+
+
+
+  @override
+  Widget build(BuildContext context) {
+   
+
+
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+       
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder(
+        future: getArticles(),
+        builder: (BuildContext context, snapshot){
+          return SingleChildScrollView(
+            child: Column(children: [
+              Text('Top Rugby Stories'),
+ListView.builder(
+  shrinkWrap: true,
+  itemCount: topStories.length,
+  itemBuilder: (context, index){
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Card(
+      
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(topStories[index]['enclosure']['url'],height: 50),
+            ),
+            SizedBox(
+              
+              width: 280,child: Text(
+              
+              topStories[index]['title']['\$t'],maxLines: 2,overflow: TextOverflow.ellipsis,softWrap: true,),)
+            
+          ],
+        ),
+      ),
+    ),
+  );
+},)
+            ]),
+          ); 
+        })
+     
+    );
+  }
+}
