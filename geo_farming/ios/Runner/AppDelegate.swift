f305c36664967d39900fd6c988a7a12ec403453f
antonwentzel <anton@idigisolweb.com>
2024-01-31 11:54:52 +0200

    update

--- geo_farming/ios/Runner/AppDelegate.swift
+++ geo_farming/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBs4FaivC2qGfOk6HST8S9rcj3ZSYHgHi0")
+    GMSServices.provideAPIKey("YOUR API KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
