e0bc358e217678f5da78f7f92e88d2bdc24bed0d
antonwentzel <anton@idigisolweb.com>
2024-02-19 10:24:46 +0200

    update

--- flutter_map_type/ios/Runner/AppDelegate.swift
+++ flutter_map_type/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDVDwldPAO2EtFg3L4-bvYpLSJkrCqhe28")
+    GMSServices.provideAPIKey("YOUR API KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
